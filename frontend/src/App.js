53bd13e2dcfa5f0dd388a3f39437507cb5cfb09c
Dariusz-Wolontariusz <darek.ciazynski@protonmail.com>
2023-02-14 19:41:33 +0100

    Header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
-// import React from 'react';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
-  return <>Welcome to my Store</>;
-};
+  return (
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to my Store</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  )
+}
 
-export default App;
+export default App
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>DreamShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link href='/cart'>
+                <i class='fa-light fa-cart-shopping'></i>Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i class='fa-regular fa-user'></i>Sign in
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
