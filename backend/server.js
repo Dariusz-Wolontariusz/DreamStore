6fb53c3409a06ec94fa274e3d0046c5bdaef75a3
Dariusz-Wolontariusz <darek.ciazynski@protonmail.com>
2023-04-17 14:25:17 +0200

    many changes to updated router dom, prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
+// sugestia chat gtp
+import { fileURLToPath } from 'url';
+import path from 'path';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 dotenv.config()
 
 connectDB()
@@ -14,9 +19,10 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
+// moved down before deplopyment
+// app.get('/', (req, res) => {
+//   res.send('API is running...')
+// })
 
 //mounting productRoutes
 
@@ -27,6 +33,16 @@ app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 
 // custom error handler
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname,'../frontend/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 
 app.use(notFound)
 
