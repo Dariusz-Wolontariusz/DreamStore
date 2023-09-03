fa5302537a74154b28c4160fd5759ae848e991b7
Dariusz-Wolontariusz <darek.ciazynski@protonmail.com>
2023-09-03 17:46:43 +0200

    after a long break

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -9,20 +9,27 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  // userLogin: userLoginReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItem'))
   : []
 
+const userFromStorege = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
+
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userFromStorege },
 }
 
 const middleware = [thunk]
