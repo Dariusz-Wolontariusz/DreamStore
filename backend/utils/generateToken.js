d069b7f4e4a97597d7a029d4b3ea927d36e73604
Dariusz-Wolontariusz <darek.ciazynski@protonmail.com>
2023-12-22 15:15:14 +0100

    CHanged cookie sameSite to strict, still cors and sameSite cookie issue

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -12,8 +12,8 @@ const generateToken = (res, userId) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'developement',
-    sameSite: 'None',
-    secure: 'strict',
+    sameSite: 'strict',
+    // secure: 'strict',
     maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
   })
 }
