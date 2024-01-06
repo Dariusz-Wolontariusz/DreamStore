75b77172f1096025a5ff37c35dfee08199d27ca4
Dariusz-Wolontariusz <darek.ciazynski@protonmail.com>
2024-01-06 17:27:21 +0100

    trying config for deployment 1

--- backend/server.js
+++ backend/server.js
@@ -43,9 +43,26 @@ app.get('/api/config/paypal', (req, res) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 })
 
-// set folder for uploads static
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+// set folder for uploads static / moved down before deployment
+// const __dirname = path.resolve()
+// app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+// config for deplopyment
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve()
+  app.use('/uploads', express.static('/var/data/uploads'))
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  const __dirname = path.resolve()
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 // error middleware
 
